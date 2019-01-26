43a2ba5ba800350cb3ada86f85d2313cbe426c02
Robert Fenner <redragon6666@gmail.com>
2019-01-26 16:22:21 -0500

    maybe?

---
+++ Assets/Stuff/BalloonBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonBehavior : MonoBehaviour
+{
+    public ConstantForce force;
+    public int counter;
+    public const int killZone = 2000;
+    // Use this for initialization
+    void Start () {
+        force = GetComponent<ConstantForce>();
+        force.force = new Vector3(0, .25F, 0);
+        counter = 0;
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+        counter++;
+        if (counter > killZone)
+        {
+            if(this.gameObject.tag == "Balloon")
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
