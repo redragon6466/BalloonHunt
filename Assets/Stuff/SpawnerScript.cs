1cf6cdcf42092bb16d92f996d44c011ec8b273d7
Fritz Wallace <john.fritz.wallace@gmail.com>
2019-01-26 16:02:51 -0500

    Updated Spawner Scripts

--- Assets/Stuff/SpawnerScript.cs
+++ Assets/Stuff/SpawnerScript.cs
@@ -15,6 +15,10 @@ public class SpawnerScript : MonoBehaviour {
     private GameObject genesisBalloon;
     private Material redBalloon;
     private object[] colors;
+    private bool isSpawningBallons;
+    //List of balloon objects that get spawned
+    private List<GameObject> BallonList;
+
 
     // Use this for initialization
     void Start () {
@@ -26,6 +30,8 @@ public class SpawnerScript : MonoBehaviour {
         genesisBalloon = GameObject.Find("SimpleBalloon");
         machineLoc = this.transform.position;
         colors = Resources.LoadAll("/", typeof(Material));
+        BallonList = new List<GameObject>();
+        isSpawningBallons = true;
     }
     
     // Update is called once per frame
@@ -36,31 +42,46 @@ public class SpawnerScript : MonoBehaviour {
 
     void spawnControl()
     {
-        //Set a random number between 100/800
-        //if simple time reaminder is equal to number, spawn balloon
-        if (simpleTime % balloonSpawn == 0)
+        if (isSpawningBallons)
         {
-            var balloon = Instantiate(genesisBalloon, new Vector3(Random.Range(-3F,3F), 0, Random.Range(3F, 10F)), this.transform.rotation);
-            balloon.tag = "Balloon";
-            var mat = balloon.GetComponent<MeshRenderer>();
-            Random r = new Random();
-            var n = Random.Range(0, 7);
-            if (colors != null && colors.Length > 0)
+            //Set a random number between 100/800
+            //if simple time reaminder is equal to number, spawn balloon
+            if (simpleTime % balloonSpawn == 0)
             {
-                mat.material = (Material)colors[n];
-            }
-            
-            //balloon.transform.Rotate(new Vector3(-90, 0, 0));
-        }
-        //reroll random
+                var balloon = Instantiate(genesisBalloon, new Vector3(Random.Range(-3F, 3F), 0, Random.Range(3F, 10F)), this.transform.rotation);
+                BallonList.Add(balloon);
+                balloon.tag = "Balloon";
+                var mat = balloon.GetComponent<MeshRenderer>();
+                Random r = new Random();
+                var n = Random.Range(0, 7);
+                if (colors != null && colors.Length > 0)
+                {
+                    mat.material = (Material)colors[n];
+                }
 
+                //balloon.transform.Rotate(new Vector3(-90, 0, 0));
+            }
+            //reroll random
+        }
+        if (simpleTime > 1800)
+        {
+            EndLevel();
+        }
 
     }
     void MoveAI()
     {
         simpleTime += 1;
     }
-
+    void EndLevel()
+    {
+        isSpawningBallons = false;
+        foreach (GameObject balloon in BallonList)
+        {
+            Destroy(balloon);
+        }
+        BallonList = new List<GameObject>();
+    }
 
     void OnGUI()
     {
